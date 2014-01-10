6b5642c0326c2d31d843ebd188ccd3801f08bc0f
José Ladislao Lainez Ortega <thedragonladis@gmail.com>
2014-01-10 18:31:12 +0100

    Added divisors to the navigation bar.

--- header.php
+++ header.php
@@ -16,7 +16,9 @@
 				<?php
 					// Home link
 					$home_link = home_url();
+					echo "<li class='nav_bar_item nav_bar_divisor'></li>";
 					echo "<li class='nav_bar_item'><a href='$home_link'>Home</a></li>";
+					echo "<li class='nav_bar_item nav_bar_divisor'></li>";
 					$pages = get_pages();
 				 	foreach ($pages as $page) {
 				 	 	echo "<li class='nav_bar_item'>
